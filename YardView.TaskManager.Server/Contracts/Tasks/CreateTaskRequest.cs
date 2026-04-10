ff12962b0d7b68da89f7a6908c70541acf600963
Mike Moody <dcmoods@gmail.com>
2026-04-10 06:52:32 -0400

    added contracts

---
+++ YardView.TaskManager.Server/Contracts/Tasks/CreateTaskRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace YardView.TaskManager.Server.Contracts.Tasks;
+
+public class CreateTaskRequest
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string Status { get; set; } = "todo";
+
+}
---
+++ YardView.TaskManager.Server/Contracts/Tasks/UpdateTaskRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace YardView.TaskManager.Server.Contracts.Tasks;
+
+public class UpdateTaskRequest
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string Status { get; set; } = "todo";
+}
